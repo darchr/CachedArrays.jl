bdbf36eb8f010f1d5e7b0d0063d00b6965d47136
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-04-05 16:25:13 -0700

    Files generated by PkgTemplates

---
+++ src/CachedArrays.jl
@@ -0,0 +1,5 @@
+module CachedArrays
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CachedArrays
+using Test
+
+@testset "CachedArrays.jl" begin
+    # Write your own tests here.
+end
