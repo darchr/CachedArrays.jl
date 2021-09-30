2e1d48b6cd0201a4762bab8d885298be4946317d
JulianToya <jtangeles@ucdavis.edu>
2021-09-30 11:03:12 -0700

    Add NUMA allocator

--- src/allocators.jl
+++ src/allocators.jl
@@ -49,3 +49,58 @@ function aligned_alloc(sz::Integer; alignment = UInt(64))
     @assert ret == 0
     return ptr_ref[]
 end
+
+#####
+##### NUMA Allocator
+#####
+
+const NUMA_ALLOCATED_SIZES = Dict{Ptr{Nothing},Integer}()
+
+struct NUMAAllocator <: AbstractAllocator
+    nodeid::Integer
+end
+
+allocate(a::NUMAAllocator, sz) = numa_alloc(sz; nodeid = a.nodeid)
+free(::NUMAAllocator, ptr::Ptr{Nothing}, sz::Integer) = numa_free(ptr, sz)
+
+# Allocates whole pages (4096 bytes).
+# NOTE: Apparently much slower than malloc. Faster alternatives did not
+#       provide ability to allocate on specific nodes.
+function numa_alloc(sz::Integer; alignment = UInt(64), nodeid::Integer = 0)
+    # Need to round up size of allocation for alignment purposes
+    obytes = sz % alignment
+    if obytes > 0
+        sz += alignment - obytes
+    end
+
+    ptr = Ptr{Nothing}()
+    ret = ccall(
+        (
+            :numa_alloc_onnode,
+            dlpath("libnuma")
+        ),
+        Ptr{Cvoid},
+        (Csize_t, Cint),
+        sz,
+        nodeid
+    )
+    @assert ret == 0
+
+    ptr = ret
+    push!(NUMA_ALLOCATED_SIZES, ptr => sz)
+    return ptr
+end
+
+function numa_free(ptr::Ptr{Nothing}, sz::Integer)
+    pop!(NUMA_ALLOCATED_SIZES, ptr)
+    ccall(
+        (
+            :numa_free,
+            dlpath("libnuma")
+        ),
+        Cvoid,
+        (Ptr{Cvoid}, Csize_t),
+        ptr,
+        sz
+    )
+end
